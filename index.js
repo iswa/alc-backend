b23c4e29016fd72b3258b590d5055078dabcb813
ISWA TIRUPATI RAO <itirupatir@gmail.com>
2020-03-01 16:55:37 +0530

    cluster added but commented

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+//mongoose.connect('mongodb+srv://event_alc:tirupati@cluster0-qetgv.mongodb.net/event-alc?retryWrites=true&w=majority',{useNewUrlParser : true, useUnifiedTopology : true, serverSelectionTimeoutMS: 5000})
 mongoose.connect('mongodb://localhost:27017/event-alc',{useNewUrlParser : true, useUnifiedTopology : true, serverSelectionTimeoutMS: 5000})
 const cors = require('cors')
 const app = express()
